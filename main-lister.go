6e2cffd5f2111c85fa07f920965b7522fbaed6af
sanchita-07 <sanchita.mishra1718@gmail.com>
2023-06-30 19:25:23 +0530

    add podlister

--- main-lister.go
+++ main-lister.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"path/filepath"
+
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
-	"path/filepath"
 
 	// admissionv1 "k8s.io/api/admissionregistration/v1"
 	informerv1 "k8s.io/client-go/informers"
 	listerv1 "k8s.io/client-go/listers/admissionregistration/v1alpha1"
+	// v1 "k8s.io/client-go/listers/apps/v1"
+	corev1 "k8s.io/client-go/listers/core/v1"
 )
 
 func getClientSet() (*kubernetes.Clientset, error) {
@@ -42,6 +45,41 @@ func createInformerLister(clientset *kubernetes.Clientset) (cache.SharedIndexInf
 	return informer, lister
 }
 
+func createInformerListerPods(clientset *kubernetes.Clientset) (cache.SharedIndexInformer, corev1.PodLister) {
+	factory := informerv1.NewSharedInformerFactory(clientset, 0)
+	informer := factory.Core().V1().Pods().Informer()
+	lister := factory.Core().V1().Pods().Lister()
+
+	// Add event handlers for the informer
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{})
+
+	return informer, lister
+}
+
+func listPods(lister corev1.PodLister) error {
+	Pods, err := lister.List(labels.Everything())
+	if err != nil {
+		return err
+	}
+
+	for _, policy := range Pods {
+		fmt.Printf("Pod : %s\n", policy.Name)
+	}
+
+	return nil
+}
+
+// func createInformerListerDeployments(clientset *kubernetes.Clientset) (cache.SharedIndexInformer, v1.DeploymentLister) {
+// 	factory := v1.NewSharedInformerFactory(clientset, 0)
+// 	informer := factory.Apps().V1().Deployments().Informer()
+// 	lister := factory.Apps().V1().Deployments().Lister()
+
+// 	// Add event handlers for the informer
+// 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{})
+
+// 	return informer, lister
+// }
+
 func listValidatingAdmissionPolicies(lister listerv1.ValidatingAdmissionPolicyLister) error {
 	validatingAdmissionPolicies, err := lister.List(labels.Everything())
 	if err != nil {
@@ -61,20 +99,34 @@ func main() {
 		panic(err.Error())
 	}
 
-	informer, lister := createInformerLister(clientset)
+	podinformer, podlister := createInformerListerPods(clientset)
+	vapinformer, vaplister := createInformerLister(clientset)
+	
 	stopCh := make(chan struct{})
 	defer close(stopCh)
 
 	// Start the Informer
-	go informer.Run(stopCh)
+	go podinformer.Run(stopCh)
+	go vapinformer.Run(stopCh)
 
 	// Wait until the Informer is synced
-	if !cache.WaitForCacheSync(stopCh, informer.HasSynced) {
+	if !cache.WaitForCacheSync(stopCh, podinformer.HasSynced) {
+		panic("Failed to sync")
+	}
+
+	// Wait until the Informer is synced
+	if !cache.WaitForCacheSync(stopCh, vapinformer.HasSynced) {
 		panic("Failed to sync")
 	}
 
 	// List the validating admission policies
-	err = listValidatingAdmissionPolicies(lister)
+	err = listValidatingAdmissionPolicies(vaplister)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// List the pods
+	err = listPods(podlister)
 	if err != nil {
 		panic(err.Error())
 	}
